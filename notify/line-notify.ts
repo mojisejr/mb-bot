1a89d0fae48b99b622629109e0eeebb41262612c
Nonthasak Laoluerat <nonthasak.l@gmail.com>
2024-02-24 22:38:13 +0700

    upd: add error notify

--- notify/line-notify.ts
+++ notify/line-notify.ts
@@ -5,6 +5,28 @@ import qs from "qs";
 import { PriceData } from "../interfaces/price-data";
 import { config } from "../config";
 
+export async function errorNotify(message: string) {
+  try {
+    const token = process.env.notify_key as string;
+    const response = await axios.post(
+      process.env.line_uri as string,
+      qs.stringify({
+        message: message,
+      }),
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          ContentType: "application/x-www-form-urlencoded",
+        },
+      }
+    );
+    // console.log("notification response", response);
+    return true;
+  } catch (error) {
+    console.log("notification error: ", error);
+  }
+}
+
 export async function hiPriceNotify(priceData: PriceData, oldPrice: number) {
   try {
     const token = process.env.notify_key as string;
--- runner.ts
+++ runner.ts
@@ -1,18 +1,23 @@
 import { loadPageContent } from "./puppeteer/load-page-content";
 import { scrapeData } from "./scrapper/scrape-data";
 import { config } from "./config";
-import { decimalAlert, priceAlert } from "./notify/alert";
 import { priceAlertV2 } from "./notify/alert-v2";
+import { errorNotify } from "./notify/line-notify";
 
 export async function run() {
-  console.log("running ..");
-  const pageContent = await loadPageContent(config.url);
-  if (pageContent == null) return;
-  const data = await scrapeData(pageContent);
-  if (data == null) return;
-  // decimalAlert(data);
-  // priceAlert(data);
-  await priceAlertV2(data);
-
+  try {
+    console.log("running ..");
+    const pageContent = await loadPageContent(config.url);
+    if (pageContent == null) return;
+    const data = await scrapeData(pageContent);
+    if (data == null) return;
+    // decimalAlert(data);
+    // priceAlert(data);
+    await priceAlertV2(data);
+  } catch (error: any) {
+    console.log("global error: ");
+    errorNotify(`🤮 Maxbit-bot: [ERROR]: ${error.message}`);
+    console.log(error);
+  }
   // console.log(data.exchangeRate);
 }
